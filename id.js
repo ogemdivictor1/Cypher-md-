4da66b7549830e0b6eb2b8aee31dda45b9ebcb24
ogemdivictor1 <227011324+ogemdivictor1@users.noreply.github.com>
2025-11-09 21:29:59 +0100

    Update id.js

--- id.js
+++ id.js
@@ -1,17 +1,11 @@
 /**
- * CYPHER-MD ID GENERATOR
- * Generates a random alphanumeric ID of a given length.
+ * CYPHER MD BOT ID GENERATOR
+ * Generates a random alphanumeric ID
  */
-
 function makeid(length = 4) {
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
-
-  for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-
+  for (let i = 0; i < length; i++) result += chars.charAt(Math.floor(Math.random() * chars.length));
   return result;
 }
-
 module.exports = { makeid };
